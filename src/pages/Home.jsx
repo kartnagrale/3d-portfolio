bc4b04888394dbc34fd0b74db25b83177f5fae60
kartnagrale <kartikknagrale@gmail.com>
2024-06-17 01:36:55 +0530

    moving edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { useState,Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
@@ -11,6 +11,8 @@ import Plane from '../models/Plane'
       // </div> */}
 
 const Home = () => {
+  const [isRotating, setisRotating] = useState(false);
+
   const adjustIslandForScreenSize = () => {
     let screenScale=null;
     let screenPosition=[0,-6.5,-43];
@@ -24,12 +26,26 @@ const Home = () => {
     return [screenScale,screenPosition,rotation];
   }
 
+  const adjustPlaneForScreenSize = () => {
+    let screenScale,screenPosition;
+
+    if(window.innerWidth<768){
+      screenScale=[1.5,1.5,1.5];
+      screenPosition=[0,-1.5,0]
+    }else{
+      screenScale=[3,3,3];
+      screenPosition=[0,-4,-4]
+    }
+    return [screenScale,screenPosition];
+  }
+
   const [islandScale,islandPosition, islandRotation] = adjustIslandForScreenSize();
+  const [planeScale,PlanePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
       <Canvas 
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${isRotating?'cursor-grabbing':'cursor-grab'}`}
         camera={{near:0.1,far:1000}}
       >
         <Suspense fallback={<Loader/>}>
@@ -45,8 +61,15 @@ const Home = () => {
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setisRotating={setisRotating}
           />
-          <Plane/>
+          <Plane
+          isRotating={isRotating}
+          planeScale={planeScale}
+          PlanePosition={PlanePosition}
+          rotation={[0,20,0]}
+          />
         </Suspense>
       </Canvas>
     </section>
