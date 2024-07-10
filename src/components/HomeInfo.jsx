f5a7ecb35265b17b54f58f23aa1067ec460a0810
kartnagrale <kartikknagrale@gmail.com>
2024-07-11 00:52:32 +0530

    udate

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -17,7 +17,7 @@ const renderContent={
         <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
             Hi, I am <span className='font-semibold'>Kartik</span>👋🏻
             <br />
-            A Full Stack Engineer from India.
+            A Full Stack Engineer and Competitive Programmer from India.
         </h1>
     ),
     2:(
